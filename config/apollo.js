5a1c27d31fcc7932488bb4aae0c1eba4d1031e31
Juan carlos Esteban Romero <juancarlos@MacBook-Air-de-Juan.local>
2025-04-05 09:38:24 -0600

    x cambio

--- config/apollo.js
+++ config/apollo.js
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context'; // Corrige también esta importación
+import { setContext } from '@apollo/client/link/context'; 
 import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const httpLink = createHttpLink({
-        //  uri: Platform.OS === 'ios' ? 'http://localhost:3000/graphql' : 'http://10.0.2.2:3000/graphql',
-           uri:'https://takeabrake.onrender.com/graphql'
+     uri: Platform.OS === 'ios' ? 'http://localhost:3000/graphql' : 'http://10.0.2.2:3000/graphql',
+        //    uri:'https://takeabrake.onrender.com/graphql'
 });
 
 const authLink = setContext(async (_, { headers }) => {
